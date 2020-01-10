7b555aba180252ada3a576334f5abf0367a38b74
alex crisara <satcom4aircrafts@gmail.com>
2020-01-10 17:05:48 -0600

    checkpoint for 'Task 2', pulled sorting logic into it's own function, added merging logic

--- json-manipulation/src/main/java/ai/brace/Main.java
+++ json-manipulation/src/main/java/ai/brace/Main.java
@@ -2,21 +2,36 @@ package ai.brace;
 
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main
 {
     public static void main(String[] args) throws FileNotFoundException {
-        JsonResource testObj = Utils.loadJsonFile("a1.json");
+        JsonResource obj1 = Utils.loadJsonFile("a1.json");
+        JsonResource obj2 = Utils.loadJsonFile("a2.json");
+        
+        List<TextArrayMember> textData1 = obj1.textArray;
+        List<TextArrayMember> textData2 =  obj2.textArray;
 
-        Comparator<TextArrayMember> compareById = Comparator.comparing(TextArrayMember::getId);
+        List<TextArrayMember> mergedList = Stream.concat(textData1.stream(), textData2.stream())
+                .collect(Collectors.toList());
 
-        List<TextArrayMember> testList = testObj.textArray;
+        List<TextArrayMember> outpList = sortListById(mergedList);
 
-        Collections.sort(testList, compareById);
-
-        for (TextArrayMember element : testList) {
+        for (TextArrayMember element : outpList) {
             System.out.println(element.textdata);
         }
 
+    }
+
+    public static List<TextArrayMember> sortListById(List<TextArrayMember> inpList) {
+        List<TextArrayMember> tempList = inpList;
+
+        Comparator<TextArrayMember> compareById = Comparator.comparing(TextArrayMember::getId);
+
+        Collections.sort(tempList, compareById);
+
+        return tempList;
     }
 }
